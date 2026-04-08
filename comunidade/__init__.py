cf6ef2a34af653b0168de53dc31fadf22474c2b5
MarceloFantin <marcelo_fantin@yahoo.com.br>
2026-04-07 21:51:38 -0300

    07/04/2026
    alteração no init para pegar o banco de dados local ou da variavel de ambiente

--- comunidade/__init__.py
+++ comunidade/__init__.py
@@ -2,11 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "2c004d6e250bae0f82d5ad8e975d4566"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+
+if os.getenv("DATABASE_URL"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
