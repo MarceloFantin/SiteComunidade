d5569788ba78df8efe745e9b4fcf04c06ae08ed4
MarceloFantin <marcelo_fantin@yahoo.com.br>
2026-04-07 21:10:58 -0300

    teste
    testes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from comunidade import app
 
   
-
+    
 if __name__ == '__main__':
     #quando for colocar no ar tem que tirar o debug=true
     #o debug = true serve para não precisar para a aplicação e rodar novamente para atulizar o site
