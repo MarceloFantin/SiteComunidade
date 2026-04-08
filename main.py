645074c6809c6ea90772792c4c4b4a1d11e94a88
MarceloFantin <marcelo_fantin@yahoo.com.br>
2026-04-07 22:33:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from comunidade import app
-
+ 
 if __name__ == '__main__':
     #quando for colocar no ar tem que tirar o debug=true
     #o debug = true serve para não precisar para a aplicação e rodar novamente para atulizar o site
